b0d449bab72d0c9c3c2dcf9b0dd872add9f6eeba
Mason Hill <masonhillsoftware@gmail.com>
2021-01-06 21:49:40 -0700

    Solved

--- Two_Stones/twoStones.java
+++ Two_Stones/twoStones.java
@@ -1,5 +1,14 @@
 package Two_Stones;
 
+import java.util.Scanner;
+
 public class twoStones {
+    public static void main(String[]args) {
+        Scanner in = new Scanner(System.in);
+
+        int x = in.nextInt();
     
+        System.out.println((x % 2 != 0) ? "Alice" : "Bob");
+        in.close();
+    }
 }
